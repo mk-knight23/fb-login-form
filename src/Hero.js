24a15bf5edbf631a296e589bd578086f55efdff4
MK KNIHT <mk.knight970@gmail.com>
2020-11-20 15:21:54 +0530

    new commit

--- src/Hero.js
+++ src/Hero.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className='hero-container'>
-      <h1 className='fb-blue'>facebook</h1>
+      <h1 className='fb-blue'>facebook BY mk</h1>
       <h4>Connect with friends and the world around you on Facebook.</h4>
     </div>
   );
